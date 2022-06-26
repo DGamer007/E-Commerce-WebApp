64d9c40f6b684445585e52e342013312bd1ed002
DGamer007 <prajapatidhruv266@gmail.com>
2022-06-26 13:24:45 +0530

    some changes

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -1,4 +1,4 @@
-import SignupForm from '../components/SignupForm';
+import SignupForm from '../components/SignupForm.js';
 import BreadCrumb from '../components/BreadCrumb';
 
 const SignupPage = () => {
